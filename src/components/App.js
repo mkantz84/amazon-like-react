026db6ce3a41caedf4f3d59a1569abf295ff02bc
michael kantz <kantzmichael@gmail.com>
2019-01-16 14:35:51 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Products from "./products/Products.js";
+
+const App = () => {
+    return <Products />;
+};
+
+export default App;
---
+++ src/components/products/Products.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Product from "./Product";
+
+const Products = () => {
+    return <Product />;
+};
+
+export default Products;
